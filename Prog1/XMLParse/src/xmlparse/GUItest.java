25c2e1c1d6e9b95a33f4d52a2f0b82af338d4bd7
Matt Dyke <matthew.dyke@mines.sdsmt.edu>
2016-02-29 13:30:54 -0700

    Changed plot from LineChart to TimeSeriesChart

--- Prog1/XMLParse/src/xmlparse/GUItest.java
+++ Prog1/XMLParse/src/xmlparse/GUItest.java
@@ -11,52 +11,77 @@ import java.util.ArrayList;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.RegularTimePeriod;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 
 public class GUItest extends JFrame
 {
-   public GUItest( String applicationTitle , String chartTitle )
-   {
-      super(applicationTitle);
-      JFreeChart lineChart = ChartFactory.createLineChart(
-         chartTitle,
-         "Day","High Temperature",
-         createDataset( XMLParse.dailyAverages ),
-         PlotOrientation.VERTICAL,
-         true,true,false);
-         
-    ChartPanel chartPanel = new ChartPanel( lineChart );
-    chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-    setContentPane( chartPanel );
-
-    // another way to exit app when window is closed
-    addWindowListener( new WindowAdapter()
+    private XYPlot plot;
+    
+    public GUItest( String applicationTitle , String chartTitle )
     {
-        public void windowClosing( WindowEvent e )
+         super(applicationTitle);
+
+         final TimeSeriesCollection dataset1 = createHighTempDataset( XMLParse.dailyAverages, "High Temperatures" );
+         final TimeSeriesCollection dataset2 = createLowTempDataset( XMLParse.dailyAverages, "Low Temperatures" );
+         final JFreeChart chart = ChartFactory.createTimeSeriesChart(
+             "Multiple Dataset Demo 1", "Time", "Value", dataset1, true, true, false
+         );
+         this.plot = chart.getXYPlot();
+
+         this.plot.setDataset( 1, dataset2 );
+         this.plot.setRenderer( 1, new StandardXYItemRenderer());
+
+         final JPanel content = new JPanel(new BorderLayout());
+
+         final ChartPanel chartPanel = new ChartPanel(chart);
+         content.add(chartPanel);
+
+         chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+         setContentPane(content);
+
+        // another way to exit app when window is closed
+        addWindowListener( new WindowAdapter()
         {
-            System.exit( 0 );
+            public void windowClosing( WindowEvent e )
+            {
+                System.exit( 0 );
+            }
+        } );
+    }
+   
+   private TimeSeriesCollection createHighTempDataset( ArrayList<DailyStats> stats, String name )
+   {
+        TimeSeries series = new TimeSeries( name );
+        
+        for( Integer i = 0; i < stats.size(); i++ )
+        {
+            RegularTimePeriod t = new Day(stats.get(i).day, 1, 2010);
+            series.add( t, stats.get( i ).highTemp );
         }
-    } );
+      
+      return new TimeSeriesCollection(series);
+   }
+   private TimeSeriesCollection createLowTempDataset( ArrayList<DailyStats> stats, String name )
+   {
+        TimeSeries series = new TimeSeries( name );
+        
+        for( Integer i = 0; i < stats.size(); i++ )
+        {
+            RegularTimePeriod t = new Day(stats.get(i).day, 1, 2010);
+            series.add( t, stats.get( i ).lowTemp );
+        }
+      
+      return new TimeSeriesCollection(series);
    }
 
-   private DefaultCategoryDataset createDataset( ArrayList<DailyStats> stats )
-   {
-      DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-      
-      for( Integer i = 0; i < stats.size(); i++ )
-      {
-          Integer currDay = stats.get( i ).day;
-          dataset.addValue( stats.get( i ).highTemp, "High Temp", currDay.toString() );
-      }
-      
-      return dataset;
-   }
    public static void main( String[ ] args ) 
    {    
        //Parse test file
