3f4a263eb3a89a3e9d15d41ddb21247acbfc3170
Matt Dyke <matthew.dyke@mines.sdsmt.edu>
2016-02-29 18:48:29 -0700

    Updated graph to use new code.

--- Prog1/XMLParse/src/prog1/GraphPanel.java
+++ Prog1/XMLParse/src/prog1/GraphPanel.java
@@ -11,44 +11,107 @@ import java.util.ArrayList;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.RefineryUtilities;
+import java.awt.*;
+import java.awt.event.*;
+import org.jfree.chart.labels.StandardXYToolTipGenerator;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.RegularTimePeriod;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
 
 public class GraphPanel extends JPanel
 {
-   public GraphPanel( String chartTitle )
-   {
-        JFreeChart lineChart = ChartFactory.createLineChart(
-           chartTitle,
-           "Day","High Temperature",
-           createDataset( XMLParse.dailyAverages ),
-           PlotOrientation.VERTICAL,
-           true,true,false);
+    private XYPlot plot;
+    
+    public GraphPanel( String chartTitle )
+    {
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+            "Weather Statistics", "Day", "Temperature", null, true, true, false
+        );
+        this.plot = chart.getXYPlot();
+
+        //Create high and low temperature datasets
+        final TimeSeriesCollection highTempDataset = createHighTempDataset( XMLParse.dailyAverages, "High Temperatures" );
+        final TimeSeriesCollection lowTempDataset = createLowTempDataset( XMLParse.dailyAverages, "Low Temperatures" );
+        final TimeSeriesCollection avgTempDataset = createAvgTempDataset( XMLParse.dailyAverages, "Average Tempeartures" );
+
+        //Plot high temperatures
+        XYLineAndShapeRenderer highTempRenderer = createRenderer( 0, "High Temperature: ", Color.RED );
+        this.plot.setDataset( 0, highTempDataset );
+        this.plot.setRenderer( 0, highTempRenderer );
+
+        //Plot low temperatures
+        XYItemRenderer lowTempRenderer = createRenderer( 1, "Low Temperature: ", Color.BLUE );
+        this.plot.setDataset( 1, lowTempDataset );
+        this.plot.setRenderer( 1, lowTempRenderer );
+
+        //Plot average temperatures
+        XYItemRenderer avgTempRenderer = createRenderer( 0, "Average Temperature: ", Color.GREEN );
+        this.plot.setDataset( 2, avgTempDataset );       
+        this.plot.setRenderer( 2, avgTempRenderer );
          
-        ChartPanel chartPanel = new ChartPanel( lineChart );
+        ChartPanel chartPanel = new ChartPanel( chart );
         //chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
         add( chartPanel );
-   }
+    }
 
-   private DefaultCategoryDataset createDataset( ArrayList<DailyStats> stats )
+    private XYLineAndShapeRenderer createRenderer( int seriesIndex, String toolTip, Color col ) 
    {
-      DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+        //Create new renderer
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        
+        //Set renderer properties
+        renderer.setSeriesPaint( 0, col );
+        renderer.setSeriesShape( 0, new Rectangle(-1, -1, 2, 2));
+        renderer.setSeriesToolTipGenerator( 0, new StandardXYToolTipGenerator() {
+                   private static final long serialVersionUID = 1L;
+                   public String generateToolTip(XYDataset dataset, int series, int item) {
+                      String toolTipStr = toolTip + dataset.getYValue(series, item);
+                      return toolTipStr;
+                }} );
+        
+        return renderer;
+    }
+   
+   private TimeSeriesCollection createHighTempDataset( ArrayList<DailyStats> stats, String name )
+   {
+        TimeSeries series = new TimeSeries( name );
+        
+        for( Integer i = 0; i < stats.size(); i++ )
+        {
+            RegularTimePeriod t = new Day(stats.get(i).day, 1, 2010);
+            series.add( t, stats.get( i ).highTemp );
+        }
       
-      for( Integer i = 0; i < stats.size(); i++ )
-      {
-          Integer currDay = stats.get( i ).day;
-          dataset.addValue( stats.get( i ).highTemp, "High Temp", currDay.toString() );
-      }
-      
-      return dataset;
+      return new TimeSeriesCollection(series);
    }
-   public static void main( String[ ] args ) 
-   {    
-       //Parse test file
-       XMLParse.parseFile( "XMLTest.xml" );
-
-       GraphPanel chart = new GraphPanel( "Daily High Temperatures" );
-       
-       chart.setVisible( true );
+   private TimeSeriesCollection createLowTempDataset( ArrayList<DailyStats> stats, String name )
+   {
+        TimeSeries series = new TimeSeries( name );
+        
+        for( Integer i = 0; i < stats.size(); i++ )
+        {
+            RegularTimePeriod t = new Day(stats.get(i).day, 1, 2010);
+            series.add( t, stats.get( i ).lowTemp );
+        }
+      
+      return new TimeSeriesCollection(series);
+   }
+   private TimeSeriesCollection createAvgTempDataset( ArrayList<DailyStats> stats, String name )
+   {
+        TimeSeries series = new TimeSeries( name );
+        
+        for( Integer i = 0; i < stats.size(); i++ )
+        {
+            RegularTimePeriod t = new Day(stats.get(i).day, 1, 2010);
+            series.add( t, stats.get( i ).avgTemp );
+        }
+      
+      return new TimeSeriesCollection(series);
    }
 }
